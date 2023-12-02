04fb115b4f9b58fc8bd45af1b18d3027ca36c7b9
Lassi Cederlöf <lassi.cederlof@tuni.fi>
2023-12-02 16:32:48 +0200

    Day 2, part 2 final

--- 2023/day_2/day_2.py
+++ 2023/day_2/day_2.py
@@ -1,6 +1,4 @@
-def main():
-    with open("input.txt", "r") as file:
-        data = file.readlines()
+def part_one_calculation(data):
 
     possible_game_ids = []
 
@@ -38,8 +36,44 @@ def main():
         game_string, id = game.strip().split(" ")
         result += int(id)
 
-    print("result:", result)
+    print("Part one result:", result)
 
+def part_two_calculation(data):
+
+    total_power = 0
+
+    for line in data:
+        game_id, game_data = line.strip().split(":")
+        game_sets = game_data.split(";")
+
+        game_lowest_green = 1
+        game_lowest_blue = 1
+        game_lowest_red = 1
+
+        for game_set in game_sets:
+
+            colors = game_set.split(",")
+            
+            for color in colors:
+                amount, color = color.strip().split(" ")
+                if color == "red" and game_lowest_red < int(amount):
+                    game_lowest_red = int(amount)
+                elif color == "blue" and game_lowest_blue < int(amount):
+                    game_lowest_blue = int(amount)
+                elif color == "green" and game_lowest_green < int(amount):
+                    game_lowest_green = int(amount)
+
+        total_power += game_lowest_red * game_lowest_blue * game_lowest_green     
+
+    print("Part two result:", total_power)
+
+def main():
+    with open("input.txt", "r") as file:
+        data = file.readlines()
+
+    part_one_calculation(data)
+
+    part_two_calculation(data)
 
 if __name__ == "__main__":
     main()
